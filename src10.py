938c14c76eae1807baf4cb903bcb1d72988dfa19
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-03-29 13:13:56 -0300

    Morphological transformations for eliminating noise

---
+++ src10.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while(1):
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([0,0,0])
+    upper_red = np.array([120,120,120])
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+
+    kernel = np.ones((5,5), np.uint8)
+    erosion = cv2.erode(mask, kernel, iterations=1)
+    dilatation = cv2.dilate(mask, kernel, iterations=1)
+
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+    cv2.imshow('frame',frame)
+    # cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    cv2.imshow('erosion',erosion)
+    cv2.imshow('dilatation',dilatation)
+    cv2.imshow('Opening',opening)
+    cv2.imshow('Closing',closing)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
